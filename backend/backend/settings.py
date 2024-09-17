a5eb96753d141b53ac2ec943c824de1a52134e6f
Destron7 <prodp313@gmail.com>
2024-09-17 16:19:14 +0530

    backend temp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "djangorestframework",
-    
+    "rest_framework",
+    "django_bcrypt",
+    "authentication",
 ]
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -123,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
